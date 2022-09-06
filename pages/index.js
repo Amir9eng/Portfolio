9033cd8fe17f1716796216e6377affd9ff1f84f4
Amir9eng <amirmukhtarme@gmail.com>
2022-09-06 15:27:50 +0100

    Skills section

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import About from '../components/About'
 import Navbar from '../components/Navbar'
+import Skills from '../components/Skills'
 import Main from './../components/Main'
 
 export default function Home () {
@@ -14,6 +15,7 @@ export default function Home () {
       <Navbar />
       <Main />
       <About />
+      <Skills />
     </div>
   )
 }
