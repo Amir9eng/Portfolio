0b32aa6a353e43780c2c86f096a8cc79bdb47ed9
Amir9eng <amirmukhtarme@gmail.com>
2022-09-09 15:36:20 +0100

    Smooth Scrolling

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import { FaLinkedinIn, FaGithub } from 'react-icons/fa'
+import Image from 'next/image'
+import Link from 'next/link'
+import React, { useState } from 'react'
 import { AiOutlineMail } from 'react-icons/ai'
 import { BsFillPersonLinesFill } from 'react-icons/bs'
-import { HiOutlineCHevronDoubleUp } from 'react-icons/hi'
-import { Link } from 'next/link'
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa'
+import { HiOutlineChevronDoubleUp } from 'react-icons/hi'
+import ContactImg from '../public/assets/contact.jpg'
 
 const Contact = () => {
   return (
-    <div className='w-full lg:h-screen'>
-      <div className='max-w-[1240px] m-auto px-2 py-16 w-full'>
+    <div id='Contact' className='w-full lg:h-screen'>
+      <div className='max-w-[1240px] m-auto px-2 py-16 w-full '>
         <p className='text-xl tracking-widest uppercase text-[#5651e5]'>
           Contact
         </p>
@@ -16,51 +18,74 @@ const Contact = () => {
         <div className='grid lg:grid-cols-5 gap-8'>
           {/* left */}
           <div className='col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl p-4'>
-            <div className='lg:p-4 h-full'>
+            <div className='lg:p-4 h-full '>
               <div>
-                <img
+                <Image
                   className='rounded-xl hover:scale-105 ease-in duration-300'
-                  src='https://images.unsplash.com/photo-1516387938699-a93567ec168e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bGFwdG9wfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
-                  alt=''
+                  src={ContactImg}
+                  alt='/'
                 />
               </div>
               <div>
-                <h2>Muhammad Mukhtar</h2>
+                <h2 className='py-2'>Clint Briley</h2>
                 <p>Front-End Developer</p>
                 <p className='py-4'>
-                  I am avaiable for full-time or freelance positions. Contact me
-                  and Let’s talk{' '}
+                  I am available for freelance or full-time positions. Contact
+                  me and let&apos;s talk.
                 </p>
               </div>
               <div>
                 <p className='uppercase pt-8'>Connect With Me</p>
                 <div className='flex items-center justify-between py-4'>
-                  <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110  ease-in duration-300'>
-                    <FaLinkedinIn />
-                  </div>
-                  <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110  ease-in duration-300'>
-                    <FaGithub />
-                  </div>
-                  <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110  ease-in duration-300'>
+                  <a
+                    href='https://www.linkedin.com/in/clint-briley-50056920a/'
+                    target='_blank'
+                    rel='noreferrer'
+                  >
+                    <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                      <FaLinkedinIn />
+                    </div>
+                  </a>
+                  <a
+                    href='https://github.com/fireclint'
+                    target='_blank'
+                    rel='noreferrer'
+                  >
+                    <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                      <FaGithub />
+                    </div>
+                  </a>
+
+                  <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
                     <AiOutlineMail />
                   </div>
-                  <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110  ease-in duration-300'>
-                    <BsFillPersonLinesFill />
-                  </div>
+                  <Link href='/resume'>
+                    <a>
+                      <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                        <BsFillPersonLinesFill />
+                      </div>
+                    </a>
+                  </Link>
                 </div>
               </div>
             </div>
           </div>
+
           {/* right */}
           <div className='col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4'>
             <div className='p-4'>
-              <form>
+              <form
+                action='https://getform.io/f/08ebcd37-f5b5-45be-8c13-714f011ce060'
+                method='POST'
+                encType='multipart/form-data'
+              >
                 <div className='grid md:grid-cols-2 gap-4 w-full py-2'>
                   <div className='flex flex-col'>
                     <label className='uppercase text-sm py-2'>Name</label>
                     <input
-                      type='text'
                       className='border-2 rounded-lg p-3 flex border-gray-300'
+                      type='text'
+                      name='name'
                     />
                   </div>
                   <div className='flex flex-col'>
@@ -68,47 +93,53 @@ const Contact = () => {
                       Phone Number
                     </label>
                     <input
-                      type='text'
                       className='border-2 rounded-lg p-3 flex border-gray-300'
+                      type='text'
+                      name='phone'
                     />
                   </div>
                 </div>
                 <div className='flex flex-col py-2'>
-                  {' '}
                   <label className='uppercase text-sm py-2'>Email</label>
                   <input
-                    type='email'
                     className='border-2 rounded-lg p-3 flex border-gray-300'
+                    type='email'
+                    name='email'
                   />
                 </div>
                 <div className='flex flex-col py-2'>
-                  {' '}
                   <label className='uppercase text-sm py-2'>Subject</label>
                   <input
-                    type='text'
                     className='border-2 rounded-lg p-3 flex border-gray-300'
+                    type='text'
+                    name='subject'
                   />
                 </div>
                 <div className='flex flex-col py-2'>
-                  {' '}
                   <label className='uppercase text-sm py-2'>Message</label>
                   <textarea
                     className='border-2 rounded-lg p-3 border-gray-300'
                     rows='10'
+                    name='message'
                   ></textarea>
                 </div>
-                <button className='w-full text-gray-100 p-4 mt-4'>
+                <button className='w-full p-4 text-gray-100 mt-4'>
                   Send Message
                 </button>
               </form>
             </div>
           </div>
         </div>
-        <div>
+        <div className='flex justify-center py-12'>
           <Link href='/'>
-            <div>
-              <HiOutlineCHevronDoubleUp />
-            </div>
+            <a>
+              <div className='rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+                <HiOutlineChevronDoubleUp
+                  className='text-[#5651e5]'
+                  size={30}
+                />
+              </div>
+            </a>
           </Link>
         </div>
       </div>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,38 +1,57 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from 'react-icons/ai'
 import { FaGithub, FaLinkedinIn } from 'react-icons/fa'
 import { BsFillPersonLinesFill } from 'react-icons/bs'
 
 const Navbar = () => {
   const [nav, setNav] = useState(false)
+  const [shadow, setShadow] = useState(false)
 
   const handleNav = () => {
     setNav(!nav)
   }
 
+  useEffect(() => {
+    const handleShadow = () => {
+      if (window.scrollY > 90) {
+        setShadow(true)
+      }
+      setShadow(false)
+    }
+    window.addEventListener('scroll', handleShadow)
+  }, [])
+
   return (
-    <div className='fixed w-full h-20 shadow-xl z-[100] bg-[#ecf0f3]'>
+    <div
+      className={
+        shadow
+          ? 'fixed w-full h-20 shadow-xl z-[100] bg-[#ecf0f3]'
+          : 'fixed w-full h-20 z-[100] bg-[#ecf0f3]'
+      }
+    >
       <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
-        <Image src='/assets/logo.jpg' width={125} height={50} alt='' />
+        <Link href='/'>
+          <Image src='/assets/icon.jpg' width={125} height={100} alt='/' />
+        </Link>
         <div>
           <ul className='hidden md:flex'>
             <Link href='/'>
               <li className='ml-10 text-sm uppercase hover:border-b'>Home</li>
             </Link>
-            <Link href='/'>
+            <Link href='/#About'>
               <li className='ml-10 text-sm uppercase hover:border-b'>About</li>
             </Link>
-            <Link href='/'>
+            <Link href='/#Skills'>
               <li className='ml-10 text-sm uppercase hover:border-b'>Skills</li>
             </Link>
-            <Link href='/'>
+            <Link href='/#Projects'>
               <li className='ml-10 text-sm uppercase hover:border-b'>
                 Projects
               </li>
             </Link>
-            <Link href='/'>
+            <Link href='/#Contact'>
               <li className='ml-10 text-sm uppercase hover:border-b'>
                 Contact
               </li>
