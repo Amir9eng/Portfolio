1d9e2e3dd46dfea3754145d8ceb4e38ea4212c6a
admin <admin@admins-MacBook-Pro.local>
2023-02-17 23:36:11 +0100

    projkects

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,49 +1,49 @@
-import Image from 'next/image'
-import React from 'react'
-import EstateImg from '../public/assets/projects/Estate.png'
-import agencyImg from '../public/assets/projects/agency.jpg'
-import lawImg from '../public/assets/projects/law.jpg'
-import twitchImg from '../public/assets/projects/twitch.jpg'
-import Link from 'next/link'
-import ProjectItem from './ProjectItem'
+import Image from 'next/image';
+import React from 'react';
+import EstateImg from '../public/assets/projects/Estate.png';
+import propertyImg from '../public/assets/projects/property.png';
+import lawImg from '../public/assets/projects/law.jpg';
+import metaverseImg from '../public/assets/projects/metaverse.png';
+import Link from 'next/link';
+import ProjectItem from './ProjectItem';
 
 const Projects = () => {
   return (
-    <div id='Projects' className='w-full'>
-      <div className='ma-w-[1240px] mx-auto px-2 py-16'>
-        <p className='text-xl tracking-widest uppercase text-[#5651e5]'>
+    <div id="Projects" className="w-full">
+      <div className="ma-w-[1240px] mx-auto px-24 py-16">
+        <p className="text-xl tracking-widest uppercase text-[#5651e5]">
           Projects
         </p>
-        <h2 className='py-4'>What I’ve Built</h2>
-        <div className='grid md:grid-cols-2 gap-8'>
+        <h2 className="py-4">What I’ve Built</h2>
+        <div className="grid md:grid-cols-2 gap-8">
           <ProjectItem
-            title='Property Finder'
+            title="Property Finder"
             backgroundImg={EstateImg}
-            projectUrl='/estate'
-            tech='Next JS'
+            projectUrl="/estate"
+            tech="Next JS"
           />
           <ProjectItem
-            title='Agency UI'
-            backgroundImg={agencyImg}
-            projectUrl='/agency'
-            tech='Next JS'
+            title="Real Estate App"
+            backgroundImg={propertyImg}
+            projectUrl="/property"
+            tech="Next JS"
           />
           <ProjectItem
-            title='Law Parlance'
+            title="Law Parlance"
             backgroundImg={lawImg}
-            projectUrl='/law'
-            tech='React JS'
+            projectUrl="/law"
+            tech="React JS"
           />
           <ProjectItem
-            title='Twitch UI'
-            backgroundImg={twitchImg}
-            projectUrl='/property'
-            tech='React JS'
+            title="Metaverse UI"
+            backgroundImg={metaverseImg}
+            projectUrl="/metaverse"
+            tech="React JS"
           />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
