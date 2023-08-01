8aee6097da9463030bdb2ea1a1c866f564a5838c
Amir <amirmukhtarme@gmail.com>
2023-08-01 18:06:12 +0100

    projects

--- components/About.jsx
+++ components/About.jsx
@@ -1,20 +1,20 @@
-import Image from 'next/image'
-import React from 'react'
-import Link from 'next/link'
+import Image from 'next/image';
+import React from 'react';
+import Link from 'next/link';
 
 const About = () => {
   return (
-    <div id='About' className='w-full md:h-screen p-2 flex items-center py-16'>
-      <div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-8'>
-        <div className='col-span-2'>
-          <p className='uppercase text-xl tracking-widest text-[#5651e5]'>
+    <div id="About" className="w-full md:h-screen p-2 flex items-center py-16">
+      <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
+        <div className="col-span-2">
+          <p className="uppercase text-xl tracking-widest text-[#5651e5]">
             About
           </p>
-          <h2 className='py-4'>Who I Am</h2>
-          <p className='py-2 text-gray-600'>
+          <h2 className="py-4">Who I Am</h2>
+          <p className="py-2 text-gray-600">
             / I am not your average developer
           </p>
-          <p className='py-2 text-gray-600'>
+          <p className="py-2 text-gray-600">
             Growing up in a world where the use of technologies increases
             exponentially over years, I always aspired to pursue a career in IT.
             I kept being fascinated with the advent of several emerging
@@ -23,7 +23,7 @@ const About = () => {
             order to understand the basics of web pages. What i thought was just
             a time whirling turned into a love for programming
           </p>
-          <p className='py-2 text-gray-600'>
+          <p className="py-2 text-gray-600">
             Intrigued by how interesting programming can be, i was quickly drawn
             to learn more. I started learning javascript and was even more
             interested in making websites interactive. i started learning
@@ -31,22 +31,22 @@ const About = () => {
             am now spending my time building projects with ReactJs, Firebase,
             and learning new technologies.
           </p>
-          <Link href='/#Projects'>
-            <p className='py-2 text-gray-600 underline cursor-pointer'>
+          <Link href="/#Projects">
+            <p className="py-2 text-blue-600 underline cursor-pointer">
               Check out some of my latest projects
             </p>
           </Link>
         </div>
-        <div className='w-full h-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>
+        <div className="w-full h-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
           <img
-            src='https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGFwdG9wfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
-            alt='/'
-            className='rounded-xl'
+            src="https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGFwdG9wfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+            alt="/"
+            className="rounded-xl"
           ></img>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
